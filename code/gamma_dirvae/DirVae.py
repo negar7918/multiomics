fae5d8720788316e146ed225875bd3555ecb967a
Negar <negar7918@gmail.com>
2025-06-30 17:56:48 +0300

    cleaned DirVae.py.

--- code/gamma_dirvae/DirVae.py
+++ code/gamma_dirvae/DirVae.py
@@ -3,27 +3,6 @@ import torch.nn.functional as F
 import torch
 
 
-def reduce_correlation(v1, v2):
-    # Ensure the vectors are tensors
-    v1 = torch.tensor(v1, dtype=torch.float32)
-    v2 = torch.tensor(v2, dtype=torch.float32)
-
-    # Step 1: Project v2 onto v1
-    projection = (torch.dot(v2, v1) / torch.dot(v1, v1)) * v1
-
-    # Step 2: Subtract the projection from v2 to reduce correlation
-    v2_orthogonal = v2 - projection
-
-    # Optionally: Normalize both vectors if necessary (comment out if not needed)
-    v1_normalized = v1 / v1.norm()
-    v2_orthogonal_normalized = v2_orthogonal / v2_orthogonal.norm()
-
-    v1_normalized[v1_normalized < 0] = 1
-    v2_orthogonal_normalized[v2_orthogonal_normalized < 0] = 1
-
-    return v1_normalized, v2_orthogonal_normalized
-
-
 def variational_encoder_decoder(layers, input_data):
     l1, l2, l3, l4, alpha, l3_ ,l2_, l1_, rec = layers
     # We give batch_size x original_features_dim
@@ -32,7 +11,7 @@ def variational_encoder_decoder(layers, input_data):
     out = F.tanh(l3(out))
     ####### Variational part
     out = alpha(out)
-    alpha_positive = F.sigmoid(out)  # torch.abs(F.tanh(alpha)) + 0.0001 # 1 + F.relu(alpha)  # F.sigmoid(alpha)
+    alpha_positive = F.sigmoid(out)  
     u = torch.rand(alpha_positive.shape)
     temp = u.mul(alpha_positive).mul(torch.exp(lgamma(alpha_positive)))
     em = torch.pow(temp, torch.pow(alpha_positive, -1))
