4ad77db6ca19692b1978601601a1e35030df99ef
Negar <negar7918@gmail.com>
2025-06-30 17:25:45 +0300

    rewrote the Dirichlet reparam.

--- code/prod_gamma_dirvae/prodDirVae.py
+++ code/prod_gamma_dirvae/prodDirVae.py
@@ -3,27 +3,6 @@ import torch.nn.functional as F
 import torch
 
 
-def reduce_correlation(v1, v2):
-    # Ensure the vectors are tensors
-    v1 = torch.tensor(v1, dtype=torch.float32)
-    v2 = torch.tensor(v2, dtype=torch.float32)
-
-    # Step 1: Project v2 onto v1
-    projection = (torch.dot(v2, v1) / torch.dot(v1, v1)) * v1
-
-    # Step 2: Subtract the projection from v2 to reduce correlation
-    v2_orthogonal = v2 - projection
-
-    # Optionally: Normalize both vectors if necessary (comment out if not needed)
-    v1_normalized = v1 / v1.norm()
-    v2_orthogonal_normalized = v2_orthogonal / v2_orthogonal.norm()
-
-    v1_normalized[v1_normalized < 0] = 1
-    v2_orthogonal_normalized[v2_orthogonal_normalized < 0] = 1
-
-    return v1_normalized, v2_orthogonal_normalized
-
-
 def variational_encoder_decoder(layers, input_data):
     l1, l2, l3, l4, alpha_layers, l3_ ,l2_, l1_, rec = layers
     # We give batch_size x original_features_dim
@@ -52,7 +31,13 @@ def variational_encoder_decoder(layers, input_data):
             u = torch.rand(alpha_positive.shape)
             temp = u.mul(alpha_positive).mul(torch.exp(lgamma(alpha_positive)))
             em = torch.pow(temp, torch.pow(alpha_positive, -1))
-            em_normalized[j, :, k] = F.softmax(em)
+            #### the below has the same value as F.softmax(em) but we do below since we need em to be distributed as Dir
+            tempreture = torch.pow(em.sum(), -1)
+            em = em / em.sum()
+            num = em / tempreture
+            soft = torch.exp(num) / (torch.exp(num)).sum()
+            em_normalized[j, :, k] = soft
+            ####
 
     em_final = torch.flatten(em_normalized, start_dim=1)
     alphas = torch.stack(alphas_pos)
